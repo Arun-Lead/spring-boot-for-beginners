b0a7565bd71000fb2abeb09350d0b000267f9bd8
Arun-Leads <arunleads485@gmail.com>
2025-01-29 20:27:03 +0530

    adding getmapping statement

--- src/main/java/com/alibou/springdemo/student/StudentController.java
+++ src/main/java/com/alibou/springdemo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.alibou.springdemo.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
+    @GetMapping
     public List<String> findAllStudents(){
         return List.of(
           "AMST", "Hello World"
